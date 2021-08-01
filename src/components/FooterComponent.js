acd3a74a6d97006135a37d63b8fef905cd661c42
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-01 16:45:29 -0400

    css work

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -26,14 +26,14 @@ class Footer extends Component {
                             </div>
 
                             <div className="col-md-4 text-center ">
-                                <h4> made with <i className="fa fa-heart"></i> in Seattle</h4>
+                                
                                 <p>copyright ©  {year}</p>
                             </div>
                      
                             <div className="col text-left offset-1">
-                                <a role="button" className="btn btn-link" href="tel:+12063838906"><h5><i className="fa fa-phone-square fa-lg"></i> 206-383-8906</h5></a><br />
-                                <a role="button" className="btn btn-link" href="mailto: chaohg@hotmail.com"><h5><i className="fa fa-envelope-square fa-lg"></i> chaohg@hotmail.com </h5></a>
-                                <a role="button" className="btn btn-link" href="https://github.com/chaohg"><h5><i className="fa fa-github fa-lg"></i> www.github.com/chaohg </h5></a>
+                                <a role="button" className="btn btn-link" href="tel:+1(3364570568)"><h5><i className="fa fa-phone-square fa-lg"></i> (336) 457-0568</h5></a><br />
+                                <a role="button" className="btn btn-link" href="mailto: mamusstaffali@gmail.com"><h5><i className="fa fa-envelope-square fa-lg"></i> mamusstaffali@gmail.com </h5></a>
+                                <a role="button" className="btn btn-link" href="https://github.com/musstaffali/realestate-react"><h5><i className="fa fa-github fa-lg"></i> Repository </h5></a>
                             </div>
                         </div>
                     </div>
